9f94f17cfafcfb3554094943a36369e3ec38dbc7
Jeremy Zongker <jeremy@zongker.net>
2023-09-04 20:15:53 -0500

    Fixed lambda entry

--- lambda.js
+++ lambda.js
@@ -3,18 +3,24 @@ const { init } = require('./dist/app');
 const { Pool } = require('@churchapps/apihelper');
 const { Environment } = require('./dist/helpers/Environment');
 
-Environment.init(process.env.APP_ENV).then(() => {
-  Pool.initPool();
-});
+const checkPool = async () => {
+  if (!Environment.connectionString) {
+    await Environment.init(process.env.APP_ENV)
+    Pool.initPool();
+  }
+}
 
 const universal = function universal(event, context) {
-  init().then(app => {
-    const server = createServer(app);
-    return proxy(server, event, context);
+  checkPool().then(() => {
+    init().then(app => {
+      const server = createServer(app);
+      return proxy(server, event, context);
+    });
   });
 }
 
 const nightly = async (event, context) => {
+  await checkPool();
   await AutomationHelper.checkAll();
 }
 
